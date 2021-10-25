dd9e6b596fb8306c2bdd3b8f24bda1ce9372cbb2
Nguyen Thai Son <sonntgch190883@fpt.edu.vn>
2021-10-25 22:31:01 +0700

    update

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const { insertToDB, getAll, deleteObject, getDocumentById, updateDocument } = require('./databaseHandler')
-const app = express()
+const express = require('express');
 
-app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: true }))
+const app = express();
 
-const path = require('path')
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+// View engine setup
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/images'))
+
+// index
+app.get('/', async (req, res) => {
+    res.render('home')
+})
+
+// All Products page
+app.get('/allproducts', async (req, res) => {
+    res.render('allproducts')
+})
 
 
+// Insert Products
+app.post('/insert', async (req, res) => {
+    const name = req.body.txtName
+    const category = req.body.txtCategory;
+    const price = req.body.txtPrice
+    const url = req.body.txtURL;
+    if (url.length == 0) {
+        var result = await getAll("Products")
+        res.render('home', { products: result, picError: 'Phai nhap Picture!' })
+    } else {
+        //xay dung doi tuong insert
+        const obj = { name: name, price: price, picURL: url, cat: category }
+        //goi ham de insert vao DB
+        await insertToDB(obj, "Products")
+        res.redirect('/')
+    }
+})
+
+
+// Delete Products
+app.get('/delete/:id', async (req, res) => {
+    const idValue = req.params.id
+    //viet ham xoa object dua tren id
+    await deleteObject(idValue, "Products")
+    res.redirect('/')
+})
+
+
+// Update Products
 app.post('/update', async (req, res) => {
     const id = req.body.txtId
     const name = req.body.txtName
@@ -21,40 +62,19 @@ app.post('/update', async (req, res) => {
     res.redirect('/')
 })
 
-app.get('/edit/:id', async (req, res) => {
-    const idValue = req.params.id
-    //lay thong tin cu cua sp cho nguoi dung xem, sua
-    const productToEdit = await getDocumentById(idValue, "Products")
-    //hien thi ra de sua
-    res.render("edit", { product: productToEdit })
-})
 
-app.get('/', async (req, res) => {
-    var result = await getAll("Products")
-    res.render('home', { products: result })
-})
-app.get('/delete/:id', async (req, res) => {
+// Edit Products
+app.get('/edit/:id', async (req,res) => {
     const idValue = req.params.id
-    //viet ham xoa object dua tren id
-    await deleteObject(idValue, "Products")
-    res.redirect('/')
-})
-app.post('/insert', async (req, res) => {
-    const name = req.body.txtName
-    const price = req.body.txtPrice
-    const url = req.body.txtURL;
-    if (url.length == 0) {
-        var result = await getAll("Products")
-        res.render('home', { products: result, picError: 'Phai nhap Picture!' })
-    } else {
-        //xay dung doi tuong insert
-        const obj = { name: name, price: price, picURL: url }
-        //goi ham de insert vao DB
-        await insertToDB(obj, "Products")
-        res.redirect('/')
-    }
-})
+     //lay thong tin cu cua sp cho nguoi dung xem, sua
+     const productToEdit = await getDocumentById(idValue, "Products")
+     //hien thi ra de sua
+     res.render("edit", { product: productToEdit })
+ })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
-console.log('Server is running!')
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('Server started on port 3000');
+})
